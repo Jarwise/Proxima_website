d2f1067b37efb64fe105473bf6d14d1b5a27ce66
Jarwise <57717541+Jarwise@users.noreply.github.com>
2021-03-03 15:50:52 +0100

    asd

--- main.js
+++ main.js
@@ -16,5 +16,21 @@ btn.addEventListener('click', e => {
   });
 
 
+  function SwitchMenu(obj){
+    if(document.getElementById){
+    var el = document.getElementById(obj);
+    var ar = document.getElementById("masterdiv").getElementsByTagName("div"); //DynamicDrive.com change
+      if(el.style.display != "block"){ //DynamicDrive.com change
+        for (var i=0; i<ar.length; i++){
+          if (ar[i].className=="submenu") //DynamicDrive.com change
+          ar[i].style.display = "none";
+        }
+        el.style.display = "block";
+      }else{
+        el.style.display = "none";
+      }
+    }
+  }
+
 
 
