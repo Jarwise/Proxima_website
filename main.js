febeb559cafd948eac0469b8c3fdc137bb6bc541
Jarwise <57717541+Jarwise@users.noreply.github.com>
2021-03-20 17:44:23 +0100

    hmm

--- main.js
+++ main.js
@@ -12,12 +12,35 @@ function scrollFunction() {
   }
 } 
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeColor(){
+  document.querySelector('header').style.backgroundColor = getRandomColor();
+  //document.getElementById('headr').style.backgroundColor = getRandomColor();
+  document.getElementById('menu').style.backgroundColor = getRandomColor();
+  document.getElementById('AI').style.backgroundColor = getRandomColor();
+  document.getElementById('leafy').style.backgroundColor = getRandomColor();
+  document.getElementById('app').style.backgroundColor = getRandomColor();
+  document.getElementById('web').style.backgroundColor = getRandomColor();
+  document.getElementById('lol').style.backgroundColor = getRandomColor();
+  document.getElementById('yeet').style.backgroundColor = getRandomColor();
+  document.getElementById('play').style.backgroundColor = getRandomColor();
+}
+
 function play() {
     var audio = document.getElementById("audio");
     audio.play();
     document.querySelector('body').style.backgroundImage = "url('https://media.tenor.com/images/cd1156d41299507df9a15fa606e41f9a/tenor.gif')";
     document.getElementById('logo').src = "imgs/nyan_cat.gif";
     document.getElementById('leafy_img').src = "imgs/nyan_cat.gif";
+    setInterval(changeColor, 1000);
   }
   
 const btn = document.querySelector('.btn');
